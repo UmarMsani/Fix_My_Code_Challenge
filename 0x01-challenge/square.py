24f493e81b35744380d02636abc6ffc8213c396c
UmarMsani <umuhammad220@gmail.com>
2023-12-10 04:42:22 -0800

    update

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -57,4 +57,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.perimeterofmysquare())
