e4b3b8b8efa99c59e82ed1c0898c7a952ba176b1
UmarMsani <umuhammad220@gmail.com>
2023-12-12 08:54:49 -0800

    update

--- 0x01-challenge/blog/app/controllers/posts_controller.rb
+++ 0x01-challenge/blog/app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   
   def index
     @posts = Post.all.order('created_at DESC')
@@ -29,8 +29,9 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
-    if @post.update(params[:post].permit(:title, :body))
+  
+  @post = Post.where(:online => true).all.order('created_at DESC') - app/controllers/posts_controllers:
+    if @post.update(params[:post].permit(:title, :body, :online))
       redirect_to @post
     else
       render 'edit'
@@ -46,6 +47,6 @@ class PostsController < ApplicationController
 
   private
     def post_params
-      params.require(:post).permit(:title, :body)
+      params.require(:post).permit(:title, :body, :online)
     end
 end
